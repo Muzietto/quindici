a519d9cc2948ff42b67d8bbc53cdbceef25beaf7
Muzietto <marco_faustinelli@yahoo.com>
2017-02-24 05:47:56 +0100

    find empty slot + slide tiles left and right

--- js/quindici.js
+++ js/quindici.js
@@ -2,10 +2,92 @@
   'use strict';
   
   define([], function() {
-    
+    var size = 4,
+    N = size - 1,
+    model = {
+      h: null,
+      v: null
+    }
+    ;
+
+    function buildModel() {
+      var modelH = rndBidimArray(size),
+      modelV = reverseModel(modelH)
+      ;
+      return {
+        h: modelH,
+        v: modelV
+      };
+    }
+
+
+    function reverseModel(model) {
+      
+    }
+
+    function move(model, str) { // e.g. '1,2,r'
+      var pieces = str.split(','),
+      x = pieces[0],
+      y = pieces[1],
+      dir = pieces[2]
+      ;
+      return {
+        r: moveRight(model.h,x,y),
+        l: moveLeft(model.h,x,y),
+        u: moveRight(model.v,y,x),
+        d: moveLeft(model.v,y,x)        
+      }[dir];
+    }
+
+    function moveRight(model) {
+      
+    }
+
+    function moveLeft(model) {
+      
+    }
+
+    function clone(arr) {
+      return JSON.parse(JSON.stringify(arr));
+    }
+
+
+    function emptySlot(arr) {
+      return arr.findIndex(elem => (elem === 0));
+    }
+
+    function slideRight(input, startPos) {
+      var arr = clone(input),
+      es = emptySlot(arr),
+      i
+      ;
+      for (i = es; i > startPos; i--) {
+        arr[i] = arr[i - 1];
+      }
+      arr[i] = 0;
+      return arr;
+    }
+
+    function slideLeft(input, startPos) {
+      var arr = clone(input),
+      es = emptySlot(arr),
+      i
+      ;
+      for (i = es; i < startPos; i++) {
+        arr[i] = arr[i + 1];
+      }
+      arr[i] = 0;
+      return arr;
+    }
 
     return {
-      pippo: 12
+      emptySlot: emptySlot,
+      slideRight: slideRight,
+      slideLeft: slideLeft,
+      move: move,
+      moveRight: moveRight,
+      moveLeft: moveLeft,
+      reverseModel: reverseModel
     };
   });
   
--- spec/quindici_test.js
+++ spec/quindici_test.js
@@ -5,9 +5,18 @@
     var expect  = chai.expect;
 
     describe('qd', function() {
-      it('xxx', function() {
-
-        expect(QD.pippo).to.be.eql(12);
+      it('can find empty slots', function() {
+        expect(QD.emptySlot([0,2,10,1])).to.be.eql(0);
+        expect(QD.emptySlot([1,2,0,1])).to.be.eql(2);
+      });
+      it('can slide tiles right to fill empty slots', function() {
+        expect(QD.slideRight([1,2,0,10],0)).to.be.eql([0,1,2,10]);
+        expect(QD.slideRight([1,2,0,10],1)).to.be.eql([1,0,2,10]);
+      });
+      it('can slide tiles left to fill empty slots', function() {
+        expect(QD.slideLeft([0,2,10,1],2)).to.be.eql([2,10,0,1]);
+        expect(QD.slideLeft([0,2,10,1],1)).to.be.eql([2,0,10,1]);
+        expect(QD.slideLeft([0,2,10,1],3)).to.be.eql([2,10,1,0]);
       });
     });
   });
