64bafd87ac4abe734bc516fae697b5151cd5821a
Muzietto <marco_faustinelli@yahoo.com>
2017-03-13 18:25:28 +0100

    taking TWO steps back....

--- js/hyera.js
+++ js/hyera.js
@@ -11,38 +11,75 @@
     return result;
   }
 
-  function denorm(arra, acc) {
-    if (arra.length === 0) return acc;
-    var result = _clone(acc),
-    input = _clone(arra),
-    emp = arra[0][0],
-    boss = arra[0][1],
+  function denorm(relsArray, accObj, previousObj) {
+    if (relsArray.length === 0) return accObj;
+    var result = _clone(accObj),
+    previous = _clone(previousObj)
+    rels = _clone(relsArray),
+    employee = rels[0][0],
+    boss = rels[0][1],
     relationship = {},
-    currentTopDog
+    currentBoss,
+    previousBoss
     ;
-    if (Object.keys(result).length === 0) { // acc === {}
-      relationship[emp] = [];
+    if (Object.keys(result).length === 0) { // accObj, previousObj === {}
+      relationship[employee] = [];
       result[boss] = [relationship];
-      return denorm(input.slice(1), result);
+      return denorm(rels.slice(1), result, result);
     }
-    currentTopDog = Object.keys(result)[0];
-    if (currentTopDog === boss) {
-      relationship[emp] = [];
-      result[boss].push(relationship);
-      return denorm(input.slice(1), result);
+    currentBoss = name(result);
+    if (currentBoss === boss) { // push new employee && backtrack
+      relationship[employee] = [];
+      //result[boss].push(relationship);
+      previousBoss = name(previous);
+      previous[previousBoss].filter(empObj => name(empObj) === boss)[0][boss].push(relationship);
+      return denorm(rels.slice(1), previous, previous);
     }
-    if (emp === currentTopDog) {
+    if (currentBoss === employee) {
       relationship[boss] = [result];
-      return denorm(input.slice(1), relationship);
+      return denorm(rels.slice(1), relationship);
     }
-    if (result[currentTopDog].length === 0) { // backtrack
-      return denorm(input, topmost);
+    if (result[currentBoss].length === 0) { // backtrack
+      return denorm(rels, topmost);
     }
-    relationship[currentTopDog] = result[currentTopDog]
+    relationship[currentBoss] = result[currentBoss]
       .map(function(potentialBossObj) {
-        return denorm(input, potentialBossObj);
+        return denorm(rels, potentialBossObj);
       });
     return relationship;
+  }
+
+  function found(newLabel, node) {
+    if (label(node) === newLabel) return true;
+    return children(node).some(function(sub) {
+      return found(newLabel, sub);
+    });
+  }
+
+  function append(newLabel, parentLabel, node) {
+    var result = _clone(node),
+    newNode = {}
+    ;
+    newNode[newLabel] = [];
+    if (label(node) === parentLabel) {
+      children(result).push(newNode);
+      return result;
+    }
+    result[label(node)] = children(node).map(function(n) {
+      if (found(parentLabel, n)) {
+        return append(newLabel, parentLabel, n);
+      }
+      return _clone(n);
+    });
+    return result;
+  }
+
+  function label(node) {
+    return Object.keys(node)[0];
+  }
+
+  function children(node) {
+    return node[label(node)];
   }
 
   function _clone(thing) {
@@ -52,7 +89,9 @@
   define([], function() {
     return {
       denormalize: denormalize,
-      serialize: serialize
+      serialize: serialize,
+      append: append,
+      found: found
     };
   });
 })();
--- spec/hyera_test.js
+++ spec/hyera_test.js
@@ -7,77 +7,128 @@
     ;
     describe('HY', function() {
 
-  function _clone(thing) {
-    return JSON.stringify(thing);
-  }
-      it('can serialize empty objects', function() {
-        expect(HY.serialize({}))
-          .to.be.eql([]);
+      describe('can find nodes', function() {
+        it('at the top of a depth-1 tree', function() {
+          expect(HY.found('a',{'a':[]}))
+            .to.be.true;
+          expect(HY.found('b',{'a':[]}))
+            .to.be.false;
+        });
+        it('at level 1 of a depth-2 tree', function() {
+          expect(HY.found('b',{'a':[{'b':[]}]}))
+            .to.be.true;
+        });
+        it('at level 2 of a depth-3 tree', function() {
+          expect(HY.found('c',{'a':[{'b':[{'c':[]}]}]}))
+            .to.be.true;
+        });
+        it('inside a multiple-branch tree (1)', function() {
+          expect(HY.found('c',{'a':[{'b':[]},{'c':[]}]}))
+            .to.be.true;
+          expect(HY.found('d',{'a':[{'b':[]},{'c':[]}]}))
+            .to.be.false;
+        });
+        it('inside a multiple-branch tree (2)', function() {
+          expect(HY.found('d',{'a':[{'b':[]},{'c':[{'d':[]}]}]}))
+            .to.be.true;
+          expect(HY.found('e',{'a':[{'b':[]},{'c':[{'d':[]}]}]}))
+            .to.be.false;
+        });
       });
-      it('can serialize objects by their keys', function() {
-        expect(HY.serialize({'pete':'nick','barbara':'nick'}))
-          .to.be.eql([['pete','nick'],['barbara','nick']]);
+
+      describe('can append leaves', function() {
+        it('to the top node of a depth-1 tree', function() {
+          expect(HY.append('c','a',{'a':[]}))
+            .to.be.eql({'a':[{'c':[]}]});
+        });
+        it('to the top node of a depth-2 tree', function() {
+          expect(HY.append('c','a',{'a':[{'b':[]}]}))
+            .to.be.eql({'a':[{'b':[]},{'c':[]}]});
+        });
+        it('to the inner node of a depth-2 tree (1)', function() {
+          expect(HY.append('c','b',{'a':[{'b':[]}]}))
+            .to.be.eql({'a':[{'b':[{'c':[]}]}]});
+        });
+        it('to the inner node of a depth-2 tree (2)', function() {
+          expect(HY.append('d','b',{'a':[{'b':[{'c':[]}]}]}))
+            .to.be.eql({'a':[{'b':[{'c':[]},{'d':[]}]}]});
+        });
       });
-      it('can solve empty hierarchies', function() {
-        expect(HY.denormalize({}))
-          .to.be.eql({});
-      });
-      it('can solve single hierarchies', function() {
-        expect(HY.denormalize({'pete':'nick'}))
-          .to.be.eql({'nick':[{'pete':[]}]});
-      });
-      it('can solve multiple hierarchies 1', function() {
-        expect(HY.denormalize({'pete':'nick','barbara':'nick'}))
-          .to.be.eql({'nick':[{'pete':[]},{'barbara':[]}]});
-      });
-      it('can solve multiple hierarchies 2', function() {
-        expect(HY.denormalize({'pete':'nick','barbara':'nick','philip':'nick'}))
-          .to.be.eql({'nick':[{'pete':[]},{'barbara':[]},{'philip':[]}]});
-      });
-      it('can solve chained hierarchies 1', function() {
-        expect(HY.denormalize({'pete':'nick','nick':'sophie'}))
-          .to.be.eql({'sophie':[{'nick':[{'pete':[]}]}]});
-      });
-      it('can solve chained hierarchies 2', function() {
-        expect(HY.denormalize({'nick':'sophie','pete':'nick'}))
-          .to.be.eql({'sophie':[{'nick':[{'pete':[]}]}]});
-      });
-      it('can solve chained hierarchies 3', function() {
-        expect(HY.denormalize({'barbara':'nick','nick':'sophie','pete':'nick'}))
-          .to.be.eql({'sophie':[{'nick':[{'barbara':[]},{'pete':[]}]}]});
-      });
-      it('can solve chained hierarchies 4', function() {
-        expect(HY.denormalize({'pete':'nick','nick':'sophie','sophie':'jonas'}))
-          .to.be.eql({'jonas':[{'sophie':[{'nick':[{'pete':[]}]}]}]});
-      });
-      it('can solve chained hierarchies 5', function() {
-        expect(_clone(HY.denormalize({'nick':'sophie','pete':'nick','sophie':'jonas'})))
-          .to.be.eql(_clone({'jonas':[{'sophie':[{'nick':[{'pete':[]}]}]}]}));
-      });
-      it('can solve complex cases 1', function() {
-        expect(HY.denormalize({'pete':'nick','nick':'sophie','sophie':'jonas','barbara':'nick'}))
-          .to.be.eql({'jonas':[{'sophie':[{'nick':[{'pete':[]},{'barbara':[]}]}]}]});
-      });
-      it('can solve complex cases 2 - layers of bosses', function() {
-        expect(HY.denormalize({'pete':'nick','nick':'sophie','luke':'sophie','sophie':'jonas','barbara':'nick'}))
-          .to.be.eql({'jonas':[{'sophie':[{'nick':[{'pete':[]},{'barbara':[]}]},{'luke':[]}]}]});
-      });
-      it('can solve complex cases 2 - given testcase', function() {
-        expect(HY.denormalize({'pete':'nick','barbara':'nick','nick':'sophie','sophie':'jonas'}))
-          .to.be.eql({'jonas':[{'sophie':[{'nick': [{'pete':[]},{'barbara':[]}]}]}]});
-      });
-      it('can solve complex cases 3 - temporary inconsistencies', function() {
-        expect(HY.denormalize({'pete':'nick','barbara':'nick','sophie':'jonas','nick':'sophie'}))
-          .to.be.eql({'jonas':[{'sophie':[{'nick': [{'pete':[]},{'barbara':[]}]}]}]});
-      });
-      xit('can detect people with two bosses', function() {
-        expect(() => HY.denormalize({'pete':'nick','pete':'sophie'}))
-          .to.throw;
-      });
-      xit('can detect circular relationships', function() {
-        expect(() => HY.denormalize({'pete':'nick','nick':'sophie','sophie':'jonas','jonas':'pete'}))
-          .to.throw;
+
+      xdescribe('can serialize', function() {
+        it('can serialize empty objects', function() {
+          expect(HY.serialize({}))
+            .to.be.eql([]);
+        });
+        it('can serialize objects by their keys', function() {
+          expect(HY.serialize({'pete':'nick','barbara':'nick'}))
+            .to.be.eql([['pete','nick'],['barbara','nick']]);
+        });
+        it('can solve empty hierarchies', function() {
+          expect(HY.denormalize({}))
+            .to.be.eql({});
+        });
+        it('can solve single hierarchies', function() {
+          expect(HY.denormalize({'pete':'nick'}))
+            .to.be.eql({'nick':[{'pete':[]}]});
+        });
+        it('can solve multiple hierarchies 1', function() {
+          expect(HY.denormalize({'pete':'nick','barbara':'nick'}))
+            .to.be.eql({'nick':[{'pete':[]},{'barbara':[]}]});
+        });
+        it('can solve multiple hierarchies 2', function() {
+          expect(HY.denormalize({'pete':'nick','barbara':'nick','philip':'nick'}))
+            .to.be.eql({'nick':[{'pete':[]},{'barbara':[]},{'philip':[]}]});
+        });
+        it('can solve chained hierarchies 1', function() {
+          expect(HY.denormalize({'pete':'nick','nick':'sophie'}))
+            .to.be.eql({'sophie':[{'nick':[{'pete':[]}]}]});
+        });
+        it('can solve chained hierarchies 2', function() {
+          expect(HY.denormalize({'nick':'sophie','pete':'nick'}))
+            .to.be.eql({'sophie':[{'nick':[{'pete':[]}]}]});
+        });
+        it('can solve chained hierarchies 3', function() {
+          expect(HY.denormalize({'barbara':'nick','nick':'sophie','pete':'nick'}))
+            .to.be.eql({'sophie':[{'nick':[{'barbara':[]},{'pete':[]}]}]});
+        });
+        it('can solve chained hierarchies 4', function() {
+          expect(HY.denormalize({'pete':'nick','nick':'sophie','sophie':'jonas'}))
+            .to.be.eql({'jonas':[{'sophie':[{'nick':[{'pete':[]}]}]}]});
+        });
+        it('can solve chained hierarchies 5', function() {
+          expect(_clone(HY.denormalize({'nick':'sophie','pete':'nick','sophie':'jonas'})))
+            .to.be.eql(_clone({'jonas':[{'sophie':[{'nick':[{'pete':[]}]}]}]}));
+        });
+        it('can solve complex cases 1', function() {
+          expect(HY.denormalize({'pete':'nick','nick':'sophie','sophie':'jonas','barbara':'nick'}))
+            .to.be.eql({'jonas':[{'sophie':[{'nick':[{'pete':[]},{'barbara':[]}]}]}]});
+        });
+        it('can solve complex cases 2 - layers of bosses', function() {
+          expect(HY.denormalize({'pete':'nick','nick':'sophie','luke':'sophie','sophie':'jonas','barbara':'nick'}))
+            .to.be.eql({'jonas':[{'sophie':[{'nick':[{'pete':[]},{'barbara':[]}]},{'luke':[]}]}]});
+        });
+        it('can solve complex cases 2 - given testcase', function() {
+          expect(HY.denormalize({'pete':'nick','barbara':'nick','nick':'sophie','sophie':'jonas'}))
+            .to.be.eql({'jonas':[{'sophie':[{'nick': [{'pete':[]},{'barbara':[]}]}]}]});
+        });
+        it('can solve complex cases 3 - temporary inconsistencies', function() {
+          expect(HY.denormalize({'pete':'nick','barbara':'nick','sophie':'jonas','nick':'sophie'}))
+            .to.be.eql({'jonas':[{'sophie':[{'nick': [{'pete':[]},{'barbara':[]}]}]}]});
+        });
+        xit('can detect people with two bosses', function() {
+          expect(() => HY.denormalize({'pete':'nick','pete':'sophie'}))
+            .to.throw;
+        });
+        xit('can detect circular relationships', function() {
+          expect(() => HY.denormalize({'pete':'nick','nick':'sophie','sophie':'jonas','jonas':'pete'}))
+            .to.throw;
+        });
       });
     });
+
+    function _clone(thing) {
+      return JSON.stringify(thing);
+    }
   });
 })();
